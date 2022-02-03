700ad5eddcab0c455aa5fe1a61e32be749f0758b
luis-salas <salas.luis40@gmail.com>
2022-02-03 14:59:02 -0600

    test

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template  # Import Flask to allow us to create our app, and import
-app = Flask(__name__)                     # Global variable __name__ tells Flask whether or not we
+
+app = Flask(__name__) # Global variable __name__ tells Flask whether or not we
+
 @app.route('/')       
 def hello_world():
   return render_template('index.html')    # Render the template and return it!
+  
 if __name__ == "__main__":
    app.run(debug=True)
