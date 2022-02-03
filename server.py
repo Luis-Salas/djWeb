75c03a20c41c4a1fd8a09c0be08a890a69842eeb
luis-salas <salas.luis40@gmail.com>
2022-02-02 21:39:50 -0600

    change proc

--- server.py
+++ server.py
@@ -3,4 +3,5 @@ app = Flask(__name__)                     # Global variable __name__ tells Flask
 @app.route('/')       
 def hello_world():
   return render_template('index.html')    # Render the template and return it!
-app.run(host="localhost", port=8008, debug=True)
+if __name__ == "__main__":
+   app.run(debug=True)
